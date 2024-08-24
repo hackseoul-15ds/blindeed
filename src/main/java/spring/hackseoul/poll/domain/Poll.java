494b7628db180903058a1f588cbe741dee72c00e
RyooChan <fbcks97@naver.com>
2024-08-25 01:15:30 +0900

    third commit

--- src/main/java/spring/hackseoul/poll/domain/Poll.java
+++ src/main/java/spring/hackseoul/poll/domain/Poll.java
@@ -2,14 +2,13 @@ package spring.hackseoul.poll.domain;
 
 import java.util.List;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data(staticConstructor = "of")
 @Accessors(chain = true)
+@Entity
 public class Poll {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,25 +16,59 @@ public class Poll {
     private long userId;
     private String title;
     private String content;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<PollOption> poolItems;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Condition> conditions;
+
+    public Poll() {
+    }
 
     @Data(staticConstructor = "of")
     @Accessors(chain = true)
+    @Entity
     public static class PollOption {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
         private String title;
-        private int count;
+
+        @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+        private List<Condition> conditions;
+
+        public PollOption() {
+        }
     }
 
     @Data(staticConstructor = "of")
     @Accessors(chain = true)
+    @Entity
     public static class Condition {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
         private String title;
+
+        @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+        private List<ConditionValue> values;
+
+        public Condition() {
+        }
+    }
+
+    @Data(staticConstructor = "of")
+    @Accessors(chain = true)
+    @Entity
+    public static class ConditionValue {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+        private String value;
+        private long count;
+
+        public ConditionValue() {
+        }
     }
 }
--- src/main/java/spring/hackseoul/poll/service/PollService.java
+++ src/main/java/spring/hackseoul/poll/service/PollService.java
@@ -6,14 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import spring.hackseoul.poll.db.ConditionValueRepository;
 import spring.hackseoul.poll.db.PollRepository;
 import spring.hackseoul.poll.domain.Poll;
 import spring.hackseoul.poll.domain.Poll.Condition;
+
+import static java.lang.Long.*;
 
 @Service
 public class PollService {
     @Autowired
     private PollRepository pollRepository;
+
+    @Autowired
+    private ConditionValueRepository conditionValueRepository;
 
     public List<Poll> findAll() {
         return pollRepository.findAll();
@@ -26,6 +32,25 @@ public class PollService {
 
     public Poll findById(long id, String param1, String param2) {
         return pollRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void vote(Poll poll, String userId, String param1, String param2) {
+        if (!zkPassAuthorization(param1, param2)) {
+            throw new RuntimeException("Authorization failed");
+        }
+
+        poll.getPoolItems().forEach(pollOption -> {
+            pollOption.getConditions().forEach(condition ->
+                condition.getValues().forEach(conditionValue ->
+                    conditionValueRepository.findById(conditionValue.getId())
+                        .ifPresent(value -> value.setCount(value.getCount() + 1))
+                )
+            );
+        });
+
+        pollRepository.findById(poll.getId())
+            .ifPresent(poll1 -> poll1.setUserId(parseLong(userId)));
     }
 
     // TODO : zkpass authorization
