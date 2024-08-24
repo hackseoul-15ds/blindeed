3e9aba7928478d8dad14db78a23c978f4e996b84
RyooChan <fbcks97@naver.com>
2024-08-24 23:42:13 +0900

    second commit

--- src/main/java/spring/hackseoul/poll/domain/Poll.java
+++ src/main/java/spring/hackseoul/poll/domain/Poll.java
@@ -2,24 +2,37 @@ package spring.hackseoul.poll.domain;
 
 import java.util.List;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
-@Data
+@Data(staticConstructor = "of")
+@Accessors(chain = true)
 public class Poll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long userId;
     private List<PollOption> poolItems;
     private List<Condition> conditions;
 
-    @Data
+    @Data(staticConstructor = "of")
+    @Accessors(chain = true)
     public static class PollOption {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
         private String title;
         private int count;
     }
 
-    @Data
+    @Data(staticConstructor = "of")
+    @Accessors(chain = true)
     public static class Condition {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
         private String title;
     }
--- src/main/java/spring/hackseoul/user/domain/User.java
+++ src/main/java/spring/hackseoul/user/domain/User.java
@@ -1,9 +1,16 @@
 package spring.hackseoul.user.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
-@Data
+@Data(staticConstructor = "of")
+@Accessors(chain = true)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
 }
